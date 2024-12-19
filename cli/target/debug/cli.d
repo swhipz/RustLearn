19fdc75c718601fa34d2ebe7e9486a0481143415
swhipz <fredrik.jensen14@live.no>
2024-12-19 22:33:58 +0100

    testing cli

---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+C:\Users\Bruker\Desktop\Desktop_sept_24\codestuff\rust\rep\RustLearn\cli\target\debug\cli.exe: C:\Users\Bruker\Desktop\Desktop_sept_24\codestuff\rust\rep\RustLearn\cli\src\main.rs
---
+++ cli/target/debug/deps/cli.d
@@ -0,0 +1,5 @@
+C:\Users\Bruker\Desktop\Desktop_sept_24\codestuff\rust\rep\RustLearn\cli\target\debug\deps\cli.exe: src/main.rs
+
+C:\Users\Bruker\Desktop\Desktop_sept_24\codestuff\rust\rep\RustLearn\cli\target\debug\deps\cli.d: src/main.rs
+
+src/main.rs:
